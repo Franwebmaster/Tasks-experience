5632e0066564695595f6e867769e4cb78048c425
Francisco <franciscobarros754@gmail.com>
2018-08-03 20:11:33 -0300

    configurando rota do projeto

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { WelcomeComponent } from './welcome/welcome.component';
+import { PanelComponent } from './panel/panel.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+	//=====> Definindo esquema de rotas
+	{ path: '', redirectTo: '/welcome', pathMatch: 'full' }, //Rota Padrão
+	{ path: 'welcome', component: WelcomeComponent },
+	{ path: 'panel', component: PanelComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
