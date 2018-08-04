555b064b4dcfd751eebc159b7ce79a5c9f02a0b7
Francisco <franciscobarros754@gmail.com>
2018-08-03 21:16:40 -0300

    Adicionando funções para adicionar tarefa, mover e deletar

--- src/app/panel/container/container.component.ts
+++ src/app/panel/container/container.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit, ElementRef, ViewChild } from '@angular/core';
+import { saveStorage } from './saveStorage.decorator';
+
 
 @Component({
   selector: 'app-container',
@@ -6,10 +8,48 @@ import { Component, OnInit, ElementRef, ViewChild } from '@angular/core';
   styleUrls: ['./container.component.scss']
 })
 export class ContainerComponent implements OnInit {
+	@ViewChild('descricaoInput') descricaoInput : ElementRef;
 
-  constructor() { }
+	//Recebendo descricão do input
+	descricao = '';
+	//Gerando Array de tarefas
+	tarefas = [];
 
-  ngOnInit() {
+  constructor() {
+  	//Salvando Tarefas em localStorage com Decorator
+  	this.tarefas = JSON.parse(localStorage.getItem('tarefas'))  || [[], [], []];
   }
 
+  ngOnInit() {}
+
+  //Function para apresentação do modal
+  //recebendo o estado por parametro
+  modalManipulator(state) {
+  	document.getElementById('modal').style.display = state;
+  }
+
+  //Function para adicionar descricao para array tarefas
+  @saveStorage()
+  addTarefa() {
+  	if (this.descricaoInput.nativeElement.value == '') {
+  		document.getElementById('descricao-input').classList.add('erro');
+  	} else {
+  		this.tarefas[0].push(this.descricao);
+      	this.modalManipulator("none");
+      	this.descricaoInput.nativeElement.value = '';
+      	document.getElementById('descricao-input').classList.remove('erro');
+  	}
+  }
+
+  //Function para mover indice entre array tarefas
+  @saveStorage()
+  movendoTarefas(t1, t2, indice) {
+  	this.tarefas[t1].push(...this.tarefas[t2].splice(indice, 1))
+  }
+
+  //Function para remover indice do array
+  @saveStorage()
+  deletarTarefa(indice, t) {
+  	this.tarefas[t].splice(indice, 1)
+  }
 }
